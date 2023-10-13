b76759ed925a58fdf30f16141becede47754b369
汪智鹏 <1848993253@qq.com>
2023-10-13 16:12:31 +0800

    项目初始化

---
+++ screen/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import container from './components/Container.vue'
+</script>
+
+<template>
+  <container>
+    <div class="text">111</div>
+  </container>
+</template>
+
+<style scoped>
+.text {
+  color: red;
+  font-size: 14px;
+  transform: scale(0.5);
+}
+</style>
---
+++ screen/src/components/container.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="screen-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style scoped></style>
