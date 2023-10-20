842d38f660e096d52c95b1a40029a4c89835cf81
汪智鹏 <1848993253@qq.com>
2023-10-20 16:28:13 +0800

    添加loading逻辑

--- screen/src/views/screen/index.vue
+++ screen/src/views/screen/index.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="home">
-    <container :options="{width: 3840, height: 2160}">
+    <div class="loading-text" v-if="loading">数据加载中...</div>
+    <container :options="{width: 3840, height: 2160}" v-else>
       <div class="text">123</div>
     </container>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import container from '../../components/container.vue'
+
+const loading = ref(true)
+
+setTimeout(() => {
+  loading.value = false
+}, 1000)
 </script>
 
 <style lang="scss" scoped>
 .home {
   width: 100%;
   height: 100%;
-  .text {
-    color: red;
-    font-size: 140px;
-  }
+  background-color: rgb(29, 29, 29);
+  color: #fff;
+  font-size: 48px;
 }
 </style>
